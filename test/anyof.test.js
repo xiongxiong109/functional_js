8b168e4705393e6e46c976f37807853c2ff36d9a
xiongxiong109 <444122662@qq.com>
2016-10-25 09:20:29 +0800

    add test for anyof

---
+++ test/anyof.test.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var expect = require('chai').expect;
+var anyOf = require('../funcs/anyof');
+
+function T () {
+	return true;
+}
+function F () {
+	return false;
+}
+
+describe('test for anyof func', () => {
+
+	it('should be false when has no argument', () => {
+		expect(anyOf()).to.be.false;
+	});
+	
+	it('should be false when all of false', () => {
+		expect(anyOf(F,F,F)).to.be.false;
+	});
+
+	it('should be true when all of true', () => {
+		expect(anyOf(T, T)).to.be.true;
+	});
+
+	it('should be false when any of false', () => {
+		expect(anyOf(T, F, T)).to.be.true;
+	});
+
+});
